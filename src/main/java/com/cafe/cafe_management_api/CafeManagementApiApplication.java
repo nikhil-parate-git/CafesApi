8a936cda37ab4cd29b806ebb454ab3b6dad15eec
nikhil parate <nikhilparate@gmail.com>
2025-12-02 21:41:37 +0530

    Added final

--- src/main/java/com/cafe/cafe_management_api/CafeManagementApiApplication.java
+++ src/main/java/com/cafe/cafe_management_api/CafeManagementApiApplication.java
@@ -2,8 +2,12 @@ package com.cafe.cafe_management_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.cafe.cafe_management_api.model") // ये add करो
+@EnableJpaRepositories(basePackages = "com.cafe.cafe_management_api.repository")
 public class CafeManagementApiApplication {
 
 	public static void main(String[] args) {
